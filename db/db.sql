b8c618eee23a017f40cbf1dcf34aa9f7f7b2ee98
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-03-03 12:34:27 +0000

    Woops

--- db/db.sql
+++ db/db.sql
@@ -1,20 +1,20 @@
 CREATE DATABASE if not exists pn_DB;
 
 
-CREATE TABLE if not exists priceNegotiator.userDetails(
+CREATE TABLE if not exists pn_DB.userDetails(
   user_id VARCHAR(25) NOT NULL PRIMARY KEY,
   user_name VARCHAR(64) NOT NULL,
   user_email VARCHAR(64) NOT NULL,
   user_picture VARCHAR(1000) NOT NULL
 ) ENGINE=INNODB;
 
-CREATE TABLE if not exists priceNegotiator.userCookies(
+CREATE TABLE if not exists pn_DB.userCookies(
   user_id VARCHAR(25) NOT NULL,
   cookie_data VARCHAR(1000) NOT NULL,
   FOREIGN KEY (user_id) REFERENCES userDetails(user_id) ON DELETE CASCADE
 ) ENGINE=INNODB;
 
-CREATE TABLE if not exists priceNegotiator.productDetails(
+CREATE TABLE if not exists pn_DB.productDetails(
   product_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   product_name VARCHAR(64) NOT NULL,
   product_rrp VARCHAR(64) NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE if not exists priceNegotiator.productDetails(
 ) ENGINE=INNODB;
 
 
-CREATE TABLE if not exists priceNegotiator.negotiation(
+CREATE TABLE if not exists pn_DB.negotiation(
   negotiation_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   product_id int NOT NULL,
   user_id VARCHAR(25) NOT NULL,
