f367d97236e9389bdb2724c210460b590553767a
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-03-03 12:28:29 +0000

    rearranged

--- db/db.sql
+++ db/db.sql
@@ -14,6 +14,15 @@ CREATE TABLE if not exists priceNegotiator.userCookies(
   FOREIGN KEY (user_id) REFERENCES userDetails(user_id) ON DELETE CASCADE
 ) ENGINE=INNODB;
 
+CREATE TABLE if not exists priceNegotiator.productDetails(
+  product_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  product_name VARCHAR(64) NOT NULL,
+  product_rrp VARCHAR(64) NOT NULL,
+  product_lowestPrice VARCHAR(64) NOT NULL,
+  product_qty INT NOT NULL
+) ENGINE=INNODB;
+
+
 CREATE TABLE if not exists priceNegotiator.negotiation(
   negotiation_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   product_id int NOT NULL,
@@ -22,12 +31,4 @@ CREATE TABLE if not exists priceNegotiator.negotiation(
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES userDetails(user_id) ON DELETE CASCADE,
   FOREIGN KEY (product_id) REFERENCES productDetails(product_id) ON DELETE CASCADE
-) ENGINE=INNODB;
-
-CREATE TABLE if not exists priceNegotiator.productDetails(
-  product_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  product_name VARCHAR(64) NOT NULL,
-  product_rrp VARCHAR(64) NOT NULL,
-  product_lowestPrice VARCHAR(64) NOT NULL,
-  product_qty INT NOT NULL
 ) ENGINE=INNODB;
