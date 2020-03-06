df6770ad7b9809a1f108b734066190abc6b4c90d
lclarke98 <48065423+lclarke98@users.noreply.github.com>
2020-03-06 13:14:51 +0000

    added

--- src/components/header.js
+++ src/components/header.js
@@ -13,6 +13,8 @@ async function loadHeader() {
     avatar: userAvatar
   });
   ReactDOM.render(header, document.getElementById('universal-header'));
+  const content = React.createElement(ListLayout, null);
+  ReactDOM.render(content, document.getElementById('main'));
 }
 
 ; // ntoe to self make logout available
@@ -66,6 +68,41 @@ async function logout() {
     redirect: 'follow'
   });
   window.location.href = '/';
+}
+
+;
+
+class ListLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return React.createElement("div", {
+      id: "list",
+      className: "list-layout"
+    }, this.state.SOMETHING.map((name, i) => React.createElement("div", {
+      key: i.toString(),
+      className: "cmpnt-container-l padding-0810"
+    }, React.createElement("section", {
+      className: "cmpnt-name"
+    }, React.createElement("h1", null, this.state.assignments[i].title)), React.createElement("div", {
+      className: "cmpnt-seperator"
+    }), React.createElement("section", {
+      className: "cmpnt-info-l"
+    }, React.createElement("p", null, "Work Due: ", this.state.assignments[i].dueDateSubmissions), React.createElement("p", null, "Submitted: ", this.state.assignments[i].workSubmitted), React.createElement("p", null, "Reviews Due: ", this.state.assignments[i].dueDateReviews), React.createElement("p", null, "Completed: ", this.state.assignments[i].reviewsCompleted)), React.createElement("div", {
+      className: "cmpnt-seperator"
+    }), React.createElement("section", {
+      className: "cmpnt-btn-container-l"
+    }, React.createElement("div", {
+      className: "default-btns-l"
+    }, React.createElement("a", {
+      href: "",
+      className: "cmpnt-btn-l"
+    }, React.createElement("span", null, React.createElement("p", null, "Open"))))))));
+  }
+
 }
 
 ;
--- src/jsx/header.js
+++ src/jsx/header.js
@@ -15,6 +15,12 @@ async function loadHeader() {
     header,
     document.getElementById('universal-header')
   );
+
+  const content = <ListLayout />;
+  ReactDOM.render(
+    content,
+    document.getElementById('main')
+  );
 };
 
 // ntoe to self make logout available
@@ -41,22 +47,60 @@ function HeaderBar(props) {
 * gLogout() - Logs the current user out of the application via their google account.
 */
 function gLogout() {
-    const auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      logout();
-    });
-  };
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    logout();
+  });
+};
   
-  /**
-  * logout() - Logs the current user out and redirects them to the '/'.
-  */
-  async function logout() {
-    const url = '/auth/logout';
-    await fetch(url, {
-      method: 'POST',
-      credentials: 'include',
-      redirect: 'follow'
-    })
-    window.location.href = '/';
-  };
+/**
+* logout() - Logs the current user out and redirects them to the '/'.
+*/
+async function logout() {
+  const url = '/auth/logout';
+  await fetch(url, {
+    method: 'POST',
+    credentials: 'include',
+    redirect: 'follow'
+  })
+  window.location.href = '/';
+};
+
+  class ListLayout extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        product: [],
+
+      };
+    }
   
+    render () {
+      return (
+        <div id="list" className="list-layout">
+        {this.state.product.map((name, i) =>
+          <div key={i.toString()} className="cmpnt-container-l padding-0810">
+          <section className="cmpnt-name">
+          <h1>{this.state.assignments[i].title}</h1>
+          </section>
+          <div className="cmpnt-seperator"></div>
+          <section className="cmpnt-info-l">
+          <p>Work Due: {this.state.product[i].}</p>
+          <p>Submitted: {this.state.product[i].}</p>
+          <p>Reviews Due: {this.state.product[i].}</p>
+          <p>Completed: {this.state.product[i].}</p>
+          </section>
+          <div className="cmpnt-seperator"></div>
+          <section className="cmpnt-btn-container-l">
+          <div className="default-btns-l">
+          <a href="" className="cmpnt-btn-l">
+          <span><p>Open</p></span>
+          </a>
+          </div>
+          </section>
+          </div>
+        )}
+        </div>
+      );
+    }
+  };
